a7f6fc884c275cacbafc9b8d88aa979e270d52d6
Olivia Panzenböck <olivia.panzenboeck@gmx.at>
2023-10-22 00:14:18 +0200

    new functions update file

--- Uebung1/Aufgabe1.py
+++ Uebung1/Aufgabe1.py
@@ -9,46 +9,61 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 # Aufgabe 1
 
 def kNN(X_tr, y_tr, X_te, k):
 
     test_points=[]
+
     for x_test in X_te:
         distances = [dist(x_test, x_train) for x_train in X_tr] # Berechne von dem aktuellen Testpunkt die Distanzen zu allen Trainingspunkten
-        df = pd.DataFrame({'distances': distances, 'labels': y_te}) # Füge die Distanzen und die Labels der Trainigspunkte zu einem Dataframe zusammen
-        df.sort_values(by='distances', ascending=True) # Sortiere das Dataframe nach den Distanzen aufsteigend
+
+        df = pd.DataFrame({'distances': distances, 'labels': y_tr}) # Füge die Distanzen und die Labels der Trainigspunkte zu einem Dataframe zusammen
+        df = df.sort_values(by='distances', ascending=True) # Sortiere das Dataframe nach den Distanzen aufsteigend
         t = df[:k]
+
+
         if (t['labels'] == 0).sum() > (t['labels'] == 1).sum():
-            test_points.append(0)
-
+            test_points.append(int(0))
         else:
-            test_points.append(1)
+            test_points.append(int(1))
 
-    df2 = pd.DataFrame({'x': X_te[:,0], 'y': X_te[:,1], 'labels': test_points})
+    df['test_points'] = test_points
 
-
-    return(df2)
+    return(df)
 
 def dist(p1, p2):
     d = np.sqrt((p2[0]-p1[0])**2+(p2[1]-p1[1])**2)
     return(d)
 
-def build_testtrain(mk, lb, n, tr_size):
-    idx = np.random.permutation(len(mk))
+def build_testtrain(mk, lb, tr_size, seed_num):
+
+    random.seed(i_seed)
+    count = round(len(mk) * tr_size)
+    idx = random.sample(range(len(mk)),len(mk))
     train_size = int(len(mk) * tr_size)
     idx_train = idx[:train_size]
     idx_test = idx[train_size:]
+
+    # idx_train = random.sample(range(len(mk)), 100)
+    # idx_test = [x for x in range(len(mk)) if x not in idx_train]
+
     X_tr = mk[idx_train] # Merkmal training
-    y_tr = lb[idx_test] # Label training
+    y_tr = lb[idx_train] # Label training
     X_te = mk[idx_test]
     y_te = lb[idx_test] # für Korrelation
 
     return(X_tr, y_tr, X_te, y_te)
 
+
 def validate_test_points(test_p, y_te):
-    acc = (sum(test_p['labels'] == y_te) / len(y_te) * 100)
+
+
+    acc = (sum(y_te == test_p['test_points']) / len(y_te) * 100)
+
     return(acc)
+
 
 
 
@@ -56,20 +71,23 @@ def validate_test_points(test_p, y_te):
 mk = np.loadtxt('data/perceptrondata.txt')
 lb = np.loadtxt('data/perceptrontarget2.txt')
 
-# Anzahl Datensets
-n = 5
+lb=lb.astype(int)
 
-size_train = 0.5 # beide Mengen gleich groß
 
-np.random.seed(1234)
 
+n = 5 # Anzahl Datensets
+size_train = 0.5 # Größe der Trainingsmenge
 k = list(range(1, 21, 2))
+
+random.seed(1234)
+random_seeds = random.sample(range(10000000), n)
 
 results = pd.DataFrame({'k':k})
 arr = []
 i = 0
-while i < n:
-    X_tr, y_tr, X_te, y_te = build_testtrain(mk, lb, n, size_train)
+#while i < n:
+for i_seed in random_seeds:
+    X_tr, y_tr, X_te, y_te = build_testtrain(mk, lb, size_train, i_seed)
     arr = []
     for j in k:
         test_p = kNN (X_tr, y_tr, X_te, j)
---
+++ Uebung1/Test_Olivia.py
@@ -0,0 +1,13 @@
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.datasets import load_iris
+
+# te=test(X_tr, y_tr, X_te, y_te,j)
+#
+# def test(X_train, y_train, X_test, y_test,n):
+#     knn = KNeighborsClassifier(n_neighbors=n)
+#
+#     knn.fit(X_train, y_train)
+#     # Predict on dataset which model has not seen before
+#     predict=knn.predict(X_test)
+#     score=knn.score(X_test, y_test)
