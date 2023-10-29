0c743ce02949a86870572a2fbec2161334bb7fd4
Neb97 <beni.stifter@gmail.com>
2023-10-29 18:43:30 +0100

    Aufgabe 3 - Binomialverteilung - fertig

--- Uebung1/Aufgabe3.py
+++ Uebung1/Aufgabe3.py
@@ -7,17 +7,21 @@ import math as m
 # Aufgabe 3
 # Binomial Koeffizient
 
+# Die Funktion binom berechnet den Binomailkoeffizient. Ihr müssen die Parameter n und theta uebergeben werden.
+# r ist ein Vektor und geht von 0:n (natürlich in python bis n+1)
+# Der Vektor results_binom gibt dann alle P(r)-Werte als Vektor aus
+
 def binom(n,theta):
-    results = []
+    results_binom = []
 
     for r in range(n+1):
-        P = ((m.factorial(n))/(m.factorial(r)*m.factorial(n-r))) * (theta ** r) * ((1 - theta) ** (n - r))
-        results.append(P)
+        p = ((m.factorial(n))/(m.factorial(r)*m.factorial(n-r))) * (theta ** r) * ((1 - theta) ** (n - r))
+        results_binom.append(p)
 
-    return(results)
+    return(results_binom)
 
 
-test = binom(3, 0.7)
+value_binom = binom(3, 0.7)
 
-print(test)
+print(value_binom)
 
