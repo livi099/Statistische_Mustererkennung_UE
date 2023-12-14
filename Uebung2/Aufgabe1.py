200a98e3088e16b6525d51f214ae70276cac1e02
Olivia Panzenböck <olivia.panzenboeck@gmx.at>
2023-12-14 18:35:06 +0100

    changes

--- Uebung2/Aufgabe1.py
+++ Uebung2/Aufgabe1.py
@@ -2,6 +2,7 @@
 # Benjamin Stifter, 01618881
 # Olivia Panzenböck, 11775488
 
+# import
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,137 +10,100 @@ from scipy.stats import norm
 import scipy.stats as stats
 import random
 from scipy.special import gamma
+import warnings
 from scipy.stats import chi2
-import seaborn as sns
+
 
 
 # Aufgabe 1
 
-def create_subplots(sample_size, bootstrap_samples, mu, std):
+# suppress warnings
+warnings.filterwarnings('ignore')
+
+def plot_rand(name, data, sizes_samp, colors, var):
     plt.figure(figsize=(18, 12))
     x_values = np.linspace(0, 10, 10000)
-    for i, estimator in enumerate( ['Stichprobenmittel', 'Stichprobenvarianz', 'Stichprobenmeridian']):
-        statistics = calculate_statistics(bootstrap_samples[i],0)
-        j=0
-        plt.gca()
-        for s in statistics:
+    for i, estimator in enumerate(['Stichprobenmittel', 'Stichprobenvarianz', 'Stichprobenmeridian'], start=1):
+        j = 0
+        ax = plt.subplot(3, 1, i)
+        for size in sizes_samp:
 
-            ax=plt.subplot(3, 1, j + 1)
+            ax.hist(data[data['Size'] == size][estimator], bins=100, alpha=0.5,
+                    label='Stichprobengröße: ' + str(size), color=colors[j])
+            # plt.suptitle('Empirische Verteilung dreier Schätzer mit wahrer Dichteverteilung')
+            ax.grid(True)
+            ax.set_axisbelow(True)
 
-            ax.hist(s, bins=100, alpha=0.6, density=False, label='Bootstrap aus Stichprobe: ' + str(i))
-
-            #plt.set_title(f'{stat} - Stichprobengröße {sample_size}')
-            #plt.xlim([3.5,4.5])
-
-            if estimator == 'Stichprobenmittel' and j==0:
-
+            # Wahre Dichtefunktion
+            if estimator == 'Stichprobenmittel':
                 ax2 = ax.twinx()
 
                 x_values2 = (x_values - mu) / (var / size)
                 p_x = 1 / np.sqrt(2 * np.pi) * np.exp(-x_values2 ** 2 / 2) / (var / size)
-                p_x=norm.pdf(x_values, loc=mu)
+                # p_x=norm.pdf(x_values, loc=mu, scale=var / size)
 
-                #tmp = -(np.power(x_values - mu, 2)) / (2 * np.power(std, 2))
-                #p_x = (1 / (np.sqrt(2 * np.pi) * std)) * np.exp(tmp)
-
-                ax2.plot(x_values, p_x, color=colorplots[j], linestyle='--')
+                ax2.plot(x_values, p_x, color=colors[j], linestyle='--')
                 ax2.set_ylabel('p(x)')
                 ax2.set_ylim([0, 6])
-            j=j+1
+
+            elif estimator == 'Stichprobenvarianz':
+                ax2 = ax.twinx()
+
+                n = (size - 1)
+                # p_x = chi2.pdf(x_values,size-1,scale=var/n)
+
+                x_values2 = (x_values) / (var / n)
+                a = 1 / (np.power(2, (n / 2)) * gamma(n / 2))
+                p_x = (a * np.power(x_values2, (n / 2 - 1)) * np.exp(-x_values2 / 2)) / (var / n)
+
+                ax2.plot(x_values, p_x, color=colors[j], linestyle='--')
+                ax2.set_ylabel('p(x)')
+                ax2.set_ylim([0, 1])
+            j = j + 1
             ax.set_title(f'{estimator}')
-            ax.set_xlabel('Schätzwert')
+            if estimator == "Stichprobenmeridian":
+                ax.set_xlabel('Schätzwert')
             ax.set_ylabel('Häufigkeit')
-            #ax.set_xlim([2, 6])
-            ax.set_ylim([0, 60])
+            ax.set_xlim([0, 10])
+            ax.set_ylim([0, 70])
             ax.legend()
-    plt.tight_layout(rect=[0, 0, 1, 0.96])
+            plt.savefig('plots/Aufgabe1/emp_ver_' + name +'.png', format='png')
+
+        # --------------------------------------------------------------------------------------------------------------------
+        # Aufgabe b)
+        for size in sizes_samp:
+            values = data[data['Size'] == size][estimator]
+            mean = np.sum(values) / len(values)
+            std = np.sqrt(np.sum((values - mean) ** 2) / (len(values) - 1))
+
+            print(
+                f"{estimator} - Size {size} - Mittelwert aus Realisierungen: {mean:.4f}, Standardabweichung aus Realisierungen: {std:.4f}")
+
+    plt.tight_layout()
     plt.show()
 
-def calculate_statistics(samples,t):
-    means = np.sum(samples, axis=1) / np.size(samples, axis=1)
-    var_sums = (samples - means[:, np.newaxis]) ** 2
-    if t == 0:
-        vars = np.sum(var_sums, axis=1) / np.size(var_sums, axis=1)
-    elif t == 1:
-        vars = np.sum(var_sums, axis=1) / np.size(var_sums-1, axis=1)
-    else:
-        print('Nur 0 oder 1 möglich!')
+def plot_boot(size, df, mu, var, number_str):
+    plt.figure(figsize=(18, 12))
+    x_values = np.linspace(0, 10, 1000)
 
-    medians = np.median(samples, axis=1)
-    return means, vars, medians
+    for i, estimator in enumerate(['Stichprobenmittel', 'Stichprobenvarianz', 'Stichprobenmeridian'], start=1):
 
-# Seed für die Reproduzierbarkeit der Zufallszahlen setzen
-np.random.seed(235)
+        j = 0
+        ax = plt.subplot(3, 1, i)
+        for i in range(number_str):
 
-# Stichproben
-num_samples = 1000
-sample_sizes = [5, 30, 100, 500]
+            ax.hist(df[df['Stichp'] == i][estimator], bins=100, alpha=0.5, density=False, label='Bootstrap aus Stichprobe: ' + str(i), color=colors[j])
+            j = j + 1
 
-# Normalverteiung
-mu = 4
-var = 3
-std = np.sqrt(var)
-
-
-# Stichproben generieren
-stichp = {}
-
-# Aufgabe a)
-
-# DataFrame für die Schätzer erstellen
-df_estimators = pd.DataFrame()
-
-# Schleife über die Größen
-for size in sample_sizes:
-    # Stichproben generieren
-    samples = np.random.normal(loc=mu, scale=std, size=(num_samples, size))
-
-    # Stichprobemittel, Stichprobenvarianz und Stichprobenmedian berechnen
-    sample_means,sample_variances,sample_medians= statistics = calculate_statistics(samples,0)
-
-    # DataFrame für die aktuellen Schätzer erstellen
-    df_size = pd.DataFrame({
-        'Size': size,
-        'Stichprobenmittel': sample_means,
-        'Stichprobenvarianz': sample_variances,
-        'Stichprobenmeridian': sample_medians
-    })
-
-    # Der DataFrame df_estimators die neuen Daten hinzufügen
-    df_estimators = pd.concat([df_estimators, df_size])
-
-colorplots = ['green', 'blue', 'red', 'orange']
-
-plt.figure(figsize=(18, 12))
-
-x_values = np.linspace(0, 10, 10000) #????????
-
-
-# Schleife über die Schätzer
-
-for i, estimator in enumerate(['Stichprobenmittel', 'Stichprobenvarianz', 'Stichprobenmeridian'], start=1):
-    j = 0
-    ax = plt.subplot(3, 1, i)
-    for size in sample_sizes:
-
-        # plt.hist(df_estimators[df_estimators['Size'] == size][estimator], bins=100, alpha=0.5, color=colorplots[j],
-        #          edgecolor=colorplots[j], label='Stichprobengröße: ' + str(size))
-        #plt.grid(True)
-        #plt.set_axisbelow(True)
-        ax.hist(df_estimators[df_estimators['Size'] == size][estimator], bins=100, alpha=0.4, label='Umfang der Stichprobe: ' + str(size),
-                color=colorplots[j])
-        plt.suptitle('Empirische Verteilung dreier Schätzer mit wahrer Dichteverteilung')
-        ax.grid(True)
-        ax.set_axisbelow(True)
-        # Wahre Dichtefunktion plotten
+        # Wahre Dichtefunktion
         if estimator == 'Stichprobenmittel':
             ax2 = ax.twinx()
 
-            x_values2=(x_values-mu)/(var /size)
-            p_x=1/np.sqrt(2*np.pi) * np.exp(-x_values2**2/2)/(var / size)
-            #p_x=norm.pdf(x_values, loc=mu, scale=var / size)
+            x_values2 = (x_values - mu) / (var / size)
+            p_x = 1 / np.sqrt(2 * np.pi) * np.exp(-x_values2 ** 2 / 2) / (var / size)
+            # p_x=norm.pdf(x_values, loc=mu, scale=var / size)
 
-            ax2.plot(x_values, p_x, color=colorplots[j], linestyle='--')
+            ax2.plot(x_values, p_x, color=colors[j], linestyle='--')
             ax2.set_ylabel('p(x)')
             ax2.set_ylim([0, 6])
 
@@ -147,75 +111,127 @@ for i, estimator in enumerate(['Stichprobenmittel', 'Stichprobenvarianz', 'Stich
             ax2 = ax.twinx()
 
             n = (size - 1)
-            #p_x = chi2.pdf(x_values,size-1,scale=var/n)
+            # p_x = chi2.pdf(x_values,size-1,scale=var/n)
 
             x_values2 = (x_values) / (var / n)
             a = 1 / (np.power(2, (n / 2)) * gamma(n / 2))
-            p_x=(a*np.power(x_values2, (n / 2 - 1))*np.exp(-x_values2 / 2)) / (var/n)
+            p_x = (a * np.power(x_values2, (n / 2 - 1)) * np.exp(-x_values2 / 2)) / (var / n)
 
-            ax2.plot(x_values, p_x, color=colorplots[j], linestyle='--')
+            ax2.plot(x_values, p_x, color=colors[j], linestyle='--')
             ax2.set_ylabel('p(x)')
             ax2.set_ylim([0, 1])
-        j = j + 1
+
         ax.set_title(f'{estimator}')
-        ax.set_xlabel('Schätzwert')
+        if estimator == "Stichprobenmeridian":
+            ax.set_xlabel('Schätzwert')
         ax.set_ylabel('Häufigkeit')
         ax.set_xlim([0, 10])
         ax.set_ylim([0, 70])
         ax.legend()
+        plt.savefig('plots/Aufgabe1/bootstrap_' + str(size) +'.png', format='png')
 
-    # --------------------------------------------------------------------------------------------------------------------
-    # Aufgabe b)
-    for size in sample_sizes:
-        values = df_estimators[df_estimators['Size'] == size][estimator]
-        mean = np.sum(values) / len(values)
-        std = np.sqrt(np.sum((values - mean) ** 2) / (len(values) - 1))
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.show()
 
-        print(f"{estimator} - Size {size} - Mittelwert aus Realisierungen: {mean:.4f}, Standardabweichung aus Realisierungen: {std:.4f}")
 
-plt.tight_layout()
-plt.show()
+def calculate_statistics(samples,t):
+    means = np.sum(samples, axis=1) / np.size(samples, axis=1)
+    var_sums = (samples - means[:, np.newaxis]) ** 2
+    if t == 0:
+        vars = np.sum(var_sums, axis=1) / np.size(var_sums, axis=1) # wenn wahres Mittel bekannt
+    elif t == 1:
+        vars = np.sum(var_sums, axis=1) / (np.size(var_sums, axis=1)-1) ## wenn wahres Mittel nicht bekannt
+    else:
+        print('Nur 0 oder 1 möglich!')
+
+    medians = np.median(samples, axis=1)
+    return means, vars, medians
+
+# Seed für die Reproduzierbarkeit der Zufallszahlen setzen
+
+
+np.random.seed(234) 
+
+
+# Stichproben
+num_samples = 1000
+sizes_samp = [5, 30, 100, 500]
+
+# Normalverteiung
+mu = 4
+var = 3
+std = np.sqrt(var)
+
+
+# Aufgabe a) und b)
+
+# DataFrame für die Schätzer erstellen
+df_rand = pd.DataFrame()
+
+# Schleife über die Größen
+for size in sizes_samp:
+
+    # Stichproben generieren
+    samples = np.random.normal(loc=mu, scale=std, size=(num_samples, size))
+
+    # Stichprobemittel, Stichprobenvarianz und Stichprobenmedian berechnen
+    sample_means,sample_variances,sample_medians= calculate_statistics(samples,1)
+
+    # DataFrame erstellen
+    df_size = pd.DataFrame({
+        'Size': size,
+        'Stichprobenmittel': sample_means,
+        'Stichprobenvarianz': sample_variances,
+        'Stichprobenmeridian': sample_medians
+    })
+
+    # Neue Daten zum Dataframe hinzufügen
+    df_rand = pd.concat([df_rand, df_size])
+
+colors = ['green', 'blue', 'red', 'orange']
+
+name = 'rand'
+plot_rand(name, df_rand, sizes_samp, colors, var)
 
 #--------------------------------------------------------------------------------------------------------------------
 # Aufgabe c)
 
-# Z-Wert für 95%-Konfidenzintervall (zweiseitig)
-z_value = stats.norm.ppf(0.975)
-population_variance = 3
-# Grenze für das wahre Mittel
-true_mean_limit = 4
 
-for size in sample_sizes:
-    # Standardabweichung der Stichprobenmittel berechnen
-    standard_error = np.sqrt(population_variance / size)
+pop_variance = 3
+limit = 4 # Grenze für das wahre Mittel
+interval = 0.95
 
-    # Konfidenzintervall berechnen
-    confidence_interval_lower = df_estimators[df_estimators['Size'] == size]['Stichprobenmittel'] - z_value * standard_error
-    confidence_interval_upper = df_estimators[df_estimators['Size'] == size]['Stichprobenmittel'] + z_value * standard_error
+for size in sizes_samp:
+    z_value = stats.norm.ppf((1 + 0.95) / 2)
+    
+    # Standard Fehler 
+    standard_error = np.sqrt(pop_variance / size)
+
+    # Konfidenzintervall (untere und obere Grenze)
+    lower = df_rand[df_rand['Size'] == size]['Stichprobenmittel'] - z_value * standard_error
+    upper = df_rand[df_rand['Size'] == size]['Stichprobenmittel'] + z_value * standard_error
 
     # Zählen, wie viele Konfidenzintervalle das wahre Mittel 4 übersteigen
-    exceeding_intervals = np.sum((confidence_interval_lower < 4) & (confidence_interval_upper > 4))
+    exc_intervals = np.sum((lower < 4) & (upper > 4))
 
-    # Anteil berechnen und ausgeben
-    exceeding_ratio = (exceeding_intervals / num_samples) * 100
+    exceeding_ratio = (exc_intervals / num_samples) * 100 # in %
     print(f"Stichprobengröße {size}: Anteil überschrittener Konfidenzintervalle: {exceeding_ratio:.4f}")
 
 
-for size in sample_sizes:
-    t_value = stats.t.ppf(0.975, df=size-1)
+for size in sizes_samp:
+    t_value = stats.t.ppf((1 + interval) / 2, df=size-1)
 
-    # Standardabweichung der Stichprobenmittel berechnen
-    standard_error = np.sqrt(df_estimators[df_estimators['Size'] == size]['Stichprobenvarianz'] / (size-1)) #???????????????? size-1?
+    # Standard Fehler 
+    standard_error = np.sqrt(df_rand[df_rand['Size'] == size]['Stichprobenvarianz'] / (size-1)) #???????????????? size-1?
 
-    # Konfidenzintervall berechnen
-    confidence_interval_lower = df_estimators[df_estimators['Size'] == size]['Stichprobenmittel'] - z_value * standard_error
-    confidence_interval_upper = df_estimators[df_estimators['Size'] == size]['Stichprobenmittel'] + z_value * standard_error
+    # Konfidenzintervall (untere und obere Grenze)
+    lower = df_rand[df_rand['Size'] == size]['Stichprobenmittel'] - z_value * standard_error
+    upper = df_rand[df_rand['Size'] == size]['Stichprobenmittel'] + z_value * standard_error
 
     # Zählen, wie viele Konfidenzintervalle das wahre Mittel 4 übersteigen
-    exceeding_intervals = np.sum((confidence_interval_lower < 4) & (confidence_interval_upper > 4))
+    exc_intervals = np.sum((lower < 4) & (upper > 4))
 
-    # Anteil berechnen und ausgeben
-    exceeding_ratio = (exceeding_intervals / num_samples) * 100
+    exceeding_ratio = (exc_intervals / num_samples) * 100 # in %
     print(f"Stichprobengröße {size}: Anteil überschrittener Konfidenzintervalle: {exceeding_ratio:.4f}")
 
 
@@ -223,20 +239,40 @@ for size in sample_sizes:
 
 # Aufgabe d)
 
-# Erzeuge 3 verschiedene Stichproben für jeden Umfang mit je 1000 Bootstrap-Stichproben
-#bootstrap_samples = {size: [[np.random.normal(loc=mu, scale=std, size=size) for _ in range(3)] for _ in range(1000)]  for size in sample_sizes}
+# Generie bootstrap samples aus 1 und 3 Stichproben
+number_str=[1, 3]
+df_boot = pd.DataFrame()
 
-bootstrap_samples = dict()
-for size in sample_sizes:
-    bootstrap_samples[size]=dict()
-    for num_stichprobe in range(3):
-        stdprob = (np.random.normal(mu, std, size))
-        data_boot = np.zeros([num_samples, size])
-        for anzahl in range(num_samples):
-            data_boot[anzahl, :] = random.choices(stdprob, k=size)
-        bootstrap_samples[size][num_stichprobe] = data_boot
+for k in number_str:
+    for size in sizes_samp:
+        for num_stichprobe in range(k):
+            a=[]
+            stprob = (np.random.normal(mu, std, size))
+            for anzahl in range(num_samples):
+                test= random.choices(stprob, k=size)
+                a.append(test)
 
+            # Stichprobemittel, Stichprobenvarianz und Stichprobenmedian berechnen
+            sample_means, sample_variances, sample_medians = calculate_statistics(a, 1)
 
-# Erstelle Subplots für jede Stichprobengröße
-for size in sample_sizes:
-    create_subplots(size, bootstrap_samples[size], mu, std)
+            # DataFrame für die aktuellen Schätzer erstellen
+            df_size = pd.DataFrame({
+                'Stichp': int(num_stichprobe),
+                'Size': size,
+                'Stichprobenmittel': sample_means,
+                'Stichprobenvarianz': sample_variances,
+                'Stichprobenmeridian': sample_medians
+            })
+
+            # Neue Daten zum Dataframe hinzufügen
+            df_boot = pd.concat([df_boot, df_size])
+
+    # Plot für 1 Stichprobe
+    if k==1:
+        name='boot'
+        plot_rand(name, df_boot, sizes_samp, colors, var)
+
+    # Plot für 3 Stichproben
+    if k==3:
+        for size in sizes_samp:
+            plot_boot(size, df_boot.loc[df_boot['Size']==size], mu, var, k)
--- Uebung2/Aufgabe3.py
+++ Uebung2/Aufgabe3.py
@@ -13,7 +13,7 @@ import scipy.integrate as integrate
 def dichte(x, mu, std):
     p_x=[]
     for i in x:
-        p_x1 = 1/(np.sqrt(2*np.pi)*std) * np.exp(-np.power(i-mu,2)/(2*np.power(std,2)))
+        p_x1 = 1/(np.sqrt(2*np.pi)*std) * np.exp(-np.power(i-mu,2)/(2*np.power(std,2))) # S. 237
         p_x.append(p_x1)
     return p_x
 
@@ -42,7 +42,7 @@ mu_H1 = 5
 std_H1 = np.sqrt(1)
 
 # Aufgabe a)
-x_values=np.linspace(0, 13, 1000)
+x_values=np.linspace(0, 12, 1000)
 
 False_P = 1-stats.norm.cdf(x_values, loc=mu_H0, scale=std_H0)  # false positive
 True_P = 1-stats.norm.cdf(x_values, loc=mu_H1, scale=std_H1)  # true positive
@@ -50,21 +50,23 @@ True_P = 1-stats.norm.cdf(x_values, loc=mu_H1, scale=std_H1)  # true positive
 VW1=[]
 VW2=[]
 for i, j in enumerate(x_values):
-    a=(True_P[i] * p_H1[0]) / (True_P[i] * p_H1[0] + False_P[i] * p_H0[0])
+    a = (True_P[i] * p_H1[0]) / (True_P[i] * p_H1[0] + False_P[i] * p_H0[0])
     b = (True_P[i] * p_H1[1]) / (True_P[i] * p_H1[1] + False_P[i] * p_H0[1])
     VW1.append(a)
     VW2.append(b)
 
 
 plt.figure(figsize=(18, 12))
-plt.plot(x_values, VW1, color='blue', label=r'$H_0$ = 0.9, $H_1$ = 0.1')
-plt.plot(x_values, VW2, color='red', label=r'$H_0$ = 0.99, $H_1$ = 0.01')
-plt.title(r'Positiver Vorhersagewert & Entscheidungsgrenze')
+plt.plot(x_values, VW1, color='orange', label=r'$H_0$ = 0.9, $H_1$ = 0.1')
+plt.plot(x_values, VW2, color='brown', label=r'$H_0$ = 0.99, $H_1$ = 0.01')
+#plt.title(r'positiven Vorhersagewert als Funktion der Entscheidungsgrenze')
 plt.ylabel(r'$p(H_1|+)$')
 plt.xlabel(r'$x^*$')
 plt.legend()
 plt.grid(True)
+plt.savefig('plots/Aufgabe3/a.eps', format='eps')
 plt.show()
+
 
 # Aufgabe b)
 px_H0 = dichte(x_values, mu_H0, std_H0)
@@ -91,12 +93,12 @@ BFR_9 = P1_err_H0 + P2_err_H0
 BFR_99 = R1 + R2
 
 plt.figure(figsize=(18, 12))
-plt.plot(x_values, p_H0_x_9, label=r'$H_0$ = 0.9, $H_1$ = 0.1', color='green')
-plt.plot(x_values, p_H1_x_9, color='green')
-plt.plot(x_values, p_H0_x_99, color='blue')
-plt.plot(x_values, p_H1_x_99, label=r'$H_0$ = 0.99, $H_1$ = 0.01', color='blue')
-plt.plot([6.7, 6.7], [0, 1], color='red')
-plt.plot([9.1, 9.1], [0, 1], label='Entscheidungsgrenzen', color='red')
+plt.plot(x_values, p_H0_x_9, label=r'$H_0$ = 0.9, $H_1$ = 0.1', color='orange')
+plt.plot(x_values, p_H1_x_9, color='orange')
+plt.plot(x_values, p_H0_x_99, color='brown')
+plt.plot(x_values, p_H1_x_99, label=r'$H_0$ = 0.99, $H_1$ = 0.01', color='brown')
+plt.plot([6.7, 6.7], [0, 1], color='k')
+plt.plot([9.1, 9.1], [0, 1], label='Entscheidungsgrenzen', color='k')
 plt.title('Posteriors')
 plt.grid(True)
 plt.ylabel(r'$p(H_i|x)$')
@@ -108,6 +110,7 @@ print(idx1)
 print(idx2)
 print(x_values[514])
 print(x_values[698])
+plt.savefig('plots/Aufgabe3/b_3.eps', format='eps')
 plt.show()
 
 
@@ -115,8 +118,45 @@ plt.show()
 # Aufgabe c)
 plt.figure(figsize=(18, 12))
 plt.plot(False_P, True_P, color='green')
-plt.title(r'ROC-Kurve')
-plt.xlabel(r'$\alpha$ (false positive rate)')
-plt.ylabel(r'$1- \beta$ (true positive rate)')
+#plt.title(r'ROC-Kurve')
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$1 - \beta$')
 plt.grid(True)
+plt.savefig('plots/Aufgabe3/c.eps', format='eps')
+plt.show()
+
+
+
+#Test
+plt.figure(figsize=(18, 12))
+p_x_H0 = dichte(x_values, mu_H0, std_H0)
+p_x_H1 = dichte(x_values, mu_H1, std_H1)
+plt.plot(x_values, p_x_H0, label=r'$H_0$ - gesund', color='red', linestyle='--')
+plt.plot(x_values, p_x_H1, label=r'$H_1$ - infiziert', color='blue', linestyle='--')
+plt.plot(x_values, p_H0_x_9, label=r'$H_0$ = 0.9', color='red')
+plt.plot(x_values, p_H1_x_9,label=r'$H_1$ = 0.1', color='blue')
+plt.plot([6.7, 6.7], [0, 1], label='Entscheidungsgrenzen', color='k')
+plt.title(r'Wahrscheinlichkeitsfuntion $H_0$ und $H_1$')
+plt.grid(True)
+plt.ylabel(r'$p(x|H_i)$')
+plt.xlabel(r'$x$')
+plt.legend()
+plt.savefig('plots/Aufgabe3/b_1.eps', format='eps')
+plt.show()
+
+#Test
+plt.figure(figsize=(18, 12))
+p_x_H0 = dichte(x_values, mu_H0, std_H0)
+p_x_H1 = dichte(x_values, mu_H1, std_H1)
+plt.plot(x_values, p_x_H0, label=r'$H_0$ - gesund',color='red', linestyle='--')
+plt.plot(x_values, p_x_H1, label=r'$H_1$ - infiziert', color='blue', linestyle='--')
+plt.plot(x_values, p_H0_x_99, label=r'$H_0$ = 0.99', color='red')
+plt.plot(x_values, p_H1_x_99,label=r'$H_1$ = 0.01', color='blue')
+plt.plot([9.1, 9.1], [0, 1], label='Entscheidungsgrenzen', color='k')
+plt.title(r'Wahrscheinlichkeitsfuntion $H_0$ und $H_1$')
+plt.grid(True)
+plt.ylabel(r'$p(x|H_i)$')
+plt.xlabel(r'$x$')
+plt.legend()
+plt.savefig('plots/Aufgabe3/b_2.eps', format='eps')
 plt.show()
