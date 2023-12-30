6a385a8d37299fc670fd7742579e90feba6e44dc
Olivia Panzenböck <olivia.panzenboeck@gmx.at>
2023-12-30 17:13:10 +0100

    changes

--- Uebung3/Aufgabe1.py
+++ Uebung3/Aufgabe1.py
@@ -1,3 +1,63 @@
 # Statistische Musterekennung WS 2023
 # Benjamin Stifter, 01618881
 # Olivia Panzenböck, 11775488
+
+import numpy.linalg as linalg
+import numpy as np
+
+# Aufgabe a)
+np.random.seed(98)
+
+# Kovarianzmatrix
+mu = [4, 7]
+var = [12, 2]
+rho = -0.5
+cov = [[var[0], rho * np.sqrt(var[0]*var[1])], [np.sqrt(var[0]*var[1]), var[1]]] #????
+
+# Eigenwertzerlegung
+eigenwerte, eigenvektoren = linalg.eig(cov)
+
+# Inverse whitening-Transformation
+lam = np.diag(eigenwerte**-0.5)
+wt = np.dot(lam, eigenvektoren.T)
+wt_inv = np.linalg.inv(wt) #?????
+
+# Stichprobe
+stichp = np.random.normal(0, 1, 600).reshape((2, 300))
+
+# Inverse whitening-Transformation wird auf Stichprobe angewendet
+stichp_wt = np.dot(wt_inv, stichp)
+
+#------------------------------------------------------------------------
+# Aufgabe b)
+stichp_wt[0] = stichp_wt[0]+mu[0]
+stichp_wt[1] = stichp_wt[1]+mu[1]
+
+# Schätzung des Mittelwerts
+mu_stichp_wt1 = sum(stichp_wt[0]) / len(stichp_wt[0])
+mu_stichp_wt2 = sum(stichp_wt[1]) / len(stichp_wt[1])
+
+print(mu_stichp_wt1)
+print(mu_stichp_wt2)
+
+var1 = sum((stichp_wt[0]-mu_stichp_wt1)*((stichp_wt[0]-mu_stichp_wt1).T))/(len(stichp_wt[0])-1)
+var2 = sum((stichp_wt[1]-mu_stichp_wt2)*((stichp_wt[1]-mu_stichp_wt2).T))/(len(stichp_wt[1])-1)
+cov = sum((stichp_wt[0]-mu_stichp_wt1)*((stichp_wt[1]-mu_stichp_wt2).T))/(len(stichp_wt[0])-1)
+
+cov_matrix = [[var1, cov], [cov, var2]]
+corr_matrix = [[var1/(np.sqrt(var1) * np.sqrt(var1)), cov/(np.sqrt(var1) * np.sqrt(var2))], [cov/(np.sqrt(var2) * np.sqrt(var1)), var2/(np.sqrt(var2) * np.sqrt(var2))]]
+
+#------------------------------------------------------------------------
+# Aufgabe c)
+cov_matrix_pack = np.cov(stichp_wt)
+corr_matrix_pack = np.corrcoef(stichp_wt)
+
+print('cov_matrix',cov_matrix)
+print('corr_matrix',corr_matrix)
+print('cov_matrix_pack',cov_matrix_pack)
+print('corr_matrix_pack',corr_matrix_pack)
+
+
+
+
+
--- Uebung3/Aufgabe2.py
+++ Uebung3/Aufgabe2.py
@@ -1,3 +1,89 @@
 # Statistische Musterekennung WS 2023
 # Benjamin Stifter, 01618881
 # Olivia Panzenböck, 11775488
+
+import numpy as np
+import numpy.linalg as linalg
+import matplotlib.pyplot as plt
+
+def vek_rotieren(vektor, angle_deg):
+    # Funktion rotiert einen Vektor um einen bestimmten Winkel
+    # Winkel in Grad
+
+    winkel_rad = np.radians(angle_deg)
+    rot_matrix = np.array([[np.cos(winkel_rad), -np.sin(winkel_rad)],
+                        [np.sin(winkel_rad), np.cos(winkel_rad)]])
+    result = np.dot(rot_matrix, vektor)
+
+    return result
+
+
+def rayleigh_quotient(matrix, w_rot):
+    # Funktion berechnet den Rayleigh-Quotienten
+    rq = np.dot(np.dot(w_rot.T, matrix), w_rot) / np.dot(w_rot.T, w_rot)
+    scale = rq * w_rot
+
+    return rq, scale
+
+# Kovarianz- und Korrelationsmatrix aus Aufgabe III.1
+cov_matrix = [[23.4599939087314, -9.903475470150495], [-9.903475470150495, 7.9789076694751175]]
+corr_matrix = [[1.0000000000000002, -0.7238556405746981], [-0.7238556405746981, 1.0]]
+
+num_steps = 360
+winkel = np.linspace(0, 360, num_steps)
+rayleigh_cov = np.zeros(num_steps)
+rayleigh_corr = np.zeros(num_steps)
+
+# Initialisiere Arrays
+scaled_directions_cov = np.zeros((num_steps, 2))
+scaled_directions_corr = np.zeros((num_steps, 2))
+
+# Richtungsvektor
+initial_direction = np.array([1, 0])
+
+for i, winkel in enumerate(winkel):
+    rotated_direction = vek_rotieren(initial_direction, winkel)
+
+    rayleigh_cov[i], scaled_directions_cov[i, :] = rayleigh_quotient(cov_matrix, rotated_direction)
+    rayleigh_corr[i], scaled_directions_corr[i, :] = rayleigh_quotient(corr_matrix, rotated_direction)
+
+
+# Eigenwertzerlegung
+eigenwerte_cov, eigenvektoren_cov = linalg.eig(cov_matrix)
+print(eigenvektoren_cov)
+eigenvektoren_cov = eigenvektoren_cov.T
+
+eigenwerte_corr, eigenvektoren_corr = linalg.eig(corr_matrix)
+eigenvektoren_corr = eigenvektoren_corr.T
+
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.plot(scaled_directions_cov[:, 0], scaled_directions_cov[:, 1],
+           color='orange', label='Skalierte Richtungsvektoren')
+for i in range(len(eigenwerte_cov)):
+    scaled_eigenvector = eigenvektoren_cov[i] * np.sqrt(eigenwerte_cov[i])
+    print(scaled_eigenvector)
+    color = ['blue', 'green']
+    plt.plot([0, scaled_eigenvector[0]], [0, scaled_eigenvector[1]], linestyle='--', color=color[i],
+             label=f'Skalierter Eigenvektor {i + 1}')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.savefig('plots/Aufgabe2/cov.eps', format='eps')
+plt.show()
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(scaled_directions_corr[:, 0], scaled_directions_corr[:, 1],
+            color='orange', label='Skalierte Richtungsvektoren')
+for i in range(len(eigenwerte_corr)):
+    scaled_eigenvector = eigenvektoren_corr[i] * np.sqrt(eigenwerte_corr[i])
+    color = ['blue', 'green']
+    plt.plot([0, scaled_eigenvector[0]], [0, scaled_eigenvector[1]], linestyle='--', color=color[i],
+             label=f'Skalierter Eigenvektor {i + 1}')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.savefig('plots/Aufgabe2/corr.eps', format='eps')
+plt.show()
