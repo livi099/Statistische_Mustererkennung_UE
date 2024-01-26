92ff9cb5caf2e59f6cfe2e00ffa6016f1ea18a69
Olivia Panzenböck <olivia.panzenboeck@gmx.at>
2024-01-26 10:39:00 +0100

    changes

--- Uebung3/Aufgabe2.py
+++ Uebung3/Aufgabe2.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def vek_rotieren(vektor, angle_deg):
     # Funktion rotiert einen Vektor um einen bestimmten Winkel
-    # Winkel in Grad
+    # angle_deg: in Grad
 
     winkel_rad = np.radians(angle_deg)
     rot_matrix = np.array([[np.cos(winkel_rad), -np.sin(winkel_rad)],
@@ -20,14 +20,15 @@ def vek_rotieren(vektor, angle_deg):
 
 def rayleigh_quotient(matrix, w_rot):
     # Funktion berechnet den Rayleigh-Quotienten
+
     rq = np.dot(np.dot(w_rot.T, matrix), w_rot) / np.dot(w_rot.T, w_rot)
     scale = rq * w_rot
 
     return rq, scale
 
 # Kovarianz- und Korrelationsmatrix aus Aufgabe III.1
-cov_matrix = [[23.4599939087314, -9.903475470150495], [-9.903475470150495, 7.9789076694751175]]
-corr_matrix = [[1.0000000000000002, -0.7238556405746981], [-0.7238556405746981, 1.0]]
+cov_matrix = [[12.554288638599248, -2.6966026138054913], [-2.6966026138054913, 2.320157557193675]]
+corr_matrix = [[1.0, -0.499645703784571], [-0.499645703784571, 1.0]]
 
 num_steps = 360
 winkel = np.linspace(0, 360, num_steps)
@@ -35,8 +36,8 @@ rayleigh_cov = np.zeros(num_steps)
 rayleigh_corr = np.zeros(num_steps)
 
 # Initialisiere Arrays
-scaled_directions_cov = np.zeros((num_steps, 2))
-scaled_directions_corr = np.zeros((num_steps, 2))
+richtung_skaliert_cov = np.zeros((num_steps, 2))
+richtung_skaliert_corr = np.zeros((num_steps, 2))
 
 # Richtungsvektor
 initial_direction = np.array([1, 0])
@@ -44,22 +45,20 @@ initial_direction = np.array([1, 0])
 for i, winkel in enumerate(winkel):
     rotated_direction = vek_rotieren(initial_direction, winkel)
 
-    rayleigh_cov[i], scaled_directions_cov[i, :] = rayleigh_quotient(cov_matrix, rotated_direction)
-    rayleigh_corr[i], scaled_directions_corr[i, :] = rayleigh_quotient(corr_matrix, rotated_direction)
+    rayleigh_cov[i], richtung_skaliert_cov[i, :] = rayleigh_quotient(cov_matrix, rotated_direction)
+    rayleigh_corr[i], richtung_skaliert_corr[i, :] = rayleigh_quotient(corr_matrix, rotated_direction)
 
 
 # Eigenwertzerlegung
 eigenwerte_cov, eigenvektoren_cov = linalg.eig(cov_matrix)
-print(eigenvektoren_cov)
 eigenvektoren_cov = eigenvektoren_cov.T
 
 eigenwerte_corr, eigenvektoren_corr = linalg.eig(corr_matrix)
 eigenvektoren_corr = eigenvektoren_corr.T
 
-
-# Plot
+# Plot Kovarianzmatrix
 plt.figure(figsize=(10, 6))
-plt.plot(scaled_directions_cov[:, 0], scaled_directions_cov[:, 1],
+plt.plot(richtung_skaliert_cov[:, 0], richtung_skaliert_cov[:, 1],
            color='orange', label='Skalierte Richtungsvektoren')
 for i in range(len(eigenwerte_cov)):
     scaled_eigenvector = eigenvektoren_cov[i] * np.sqrt(eigenwerte_cov[i])
@@ -73,9 +72,9 @@ plt.legend()
 plt.savefig('plots/Aufgabe2/cov.eps', format='eps')
 plt.show()
 
-
+# Plot Korrelationsmatrix
 plt.figure(figsize=(10, 6))
-plt.plot(scaled_directions_corr[:, 0], scaled_directions_corr[:, 1],
+plt.plot(richtung_skaliert_corr[:, 0], richtung_skaliert_corr[:, 1],
             color='orange', label='Skalierte Richtungsvektoren')
 for i in range(len(eigenwerte_corr)):
     scaled_eigenvector = eigenvektoren_corr[i] * np.sqrt(eigenwerte_corr[i])
